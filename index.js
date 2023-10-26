823e171cc8823823fbeb20b2d9b2b38de52e34f2
cjenschke <cimarron13@mac.com>
2023-10-26 18:25:59 -0500

    working on index code

--- index.js
+++ index.js
@@ -21,3 +21,52 @@ pool
   .catch((error) => {
     console.error('Error connecting to employee_tracker_db:', error);
   });
+
+// Prompts for user input
+function start() {
+  inquirer
+    .prompt([
+      {
+        name: 'action',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+          'View all departments',
+          'View all roles',
+          'View all employees',
+          'Add a department',
+          'Add a role',
+          'Add an employee',
+          'Add an employee role',
+          'Exit',
+        ],
+      },
+    ])
+    .then((answer) => {
+      switch (answer.action) {
+        case 'View all departments':
+          viewDepartments();
+          break;
+        case 'View all roles':
+          viewRoles();
+          break;
+        case 'View all employees':
+          viewEmployees();
+          break;
+        case 'Add a department':
+          addDepartment();
+          break;
+        case 'Add an employee':
+          addEmployee();
+          break;
+        case 'Add an employee role':
+          updateEmployeeRole();
+          break;
+        case 'Exit':
+          connection.end();
+          console.log('Goodbye!');
+          pool.end();
+          break;
+      }
+    });
+}
