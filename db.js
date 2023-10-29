86ac01937c9b56b9846c497a50216436b42bffd6
cjenschke <cimarron13@mac.com>
2023-10-29 13:33:23 -0500

    added env file for username & password

--- db.js
+++ db.js
@@ -1,12 +1,21 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config();
+
+DB_USER = process.env.DB_USER;
+DB_PASSWORD = process.env.DB_PASSWORD;
 
 // Create a connection to the mysql database
 const pool = mysql.createPool({
   host: 'localhost',
   port: 3306,
-  user: 'root',
-  password: 'U3hwdyi#',
+  user: DB_USER,
+  password: DB_PASSWORD,
   database: 'employee_tracker_db',
+  // host: 'localhost',
+  // port: 3306,
+  // user: 'root',
+  // password: 'U3hwdyi#',
+  // database: 'employee_tracker_db',
 });
 // let connection;
 module.exports = pool;
